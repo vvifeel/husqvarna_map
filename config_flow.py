6da9dc1080aa1698c68f8442caf199b442d49019
vvifeel <110106147+vvifeel@users.noreply.github.com>
2026-05-08 20:41:13 +0900

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -8,11 +8,13 @@ class DolsoeMapConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title="돌쇠 하이브리드 맵", data=user_input)
+            return self.async_create_entry(title=f"돌쇠 맵 ({user_input['source_entity']})", data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
+                # 데이터를 가져올 엔티티 ID를 입력받습니다.
+                vol.Required("source_entity", default="device_tracker.dolsoe_405x"): str,
                 vol.Required("map_path", default="/config/www/maps/mower_map.png"): str,
                 vol.Required("mower_path", default="/config/www/maps/mower_icon.png"): str,
                 vol.Required("top_left", default="35.4755234, 129.2316285"): str,
